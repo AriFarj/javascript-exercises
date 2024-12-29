ffd60016e0a37b4cae8e78e012d5ed2ff74e0a63
Ari <24855858+AriFarj@users.noreply.github.com>
2024-12-29 18:01:52 +0200

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...removedItems) {
+  return array.filter(val => !removedItems.includes(val))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
