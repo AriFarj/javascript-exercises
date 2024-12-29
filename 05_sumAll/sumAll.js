bb79fa184bbcedb4feb0694f0e4da90c9d18e508
Ari <24855858+AriFarj@users.noreply.github.com>
2024-12-29 20:38:55 +0200

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, lastNum) {
+    if ((firstNum < 0 || lastNum < 0) || (!Number.isInteger(firstNum) || !(Number.isInteger(lastNum))) ) 
+        return 'ERROR';
+    
+    
+   
+    let maxNum = Math.max(firstNum, lastNum)
+    let minNum = Math.min(firstNum, lastNum)
+    return ((maxNum - minNum) + 1) * (maxNum + minNum) / 2
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
