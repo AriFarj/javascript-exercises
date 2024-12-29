01a1bfaac4f3a92fe75b9608a649933862e0f7e3
Ari <24855858+AriFarj@users.noreply.github.com>
2024-12-29 21:13:01 +0200

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenDegree) {
+  degreeConversion = (fahrenDegree -32) * (5/9)
+  celsiusDegree = Math.round(degreeConversion * 10)/10
+  return celsiusDegree;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegree) {
+  degreeConversion = (celsiusDegree * 9/5 + 32)
+  fahrenDegree = Math.round(degreeConversion * 10)/10
+  return fahrenDegree;
 };
 
 // Do not edit below this line
