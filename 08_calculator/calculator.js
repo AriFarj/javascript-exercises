8f3876c79cf9fa5cb51717d6cec2aa9cf3acea1d
Ari <24855858+AriFarj@users.noreply.github.com>
2025-01-02 14:34:52 +0200

    08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length == 0) return 0;
+  else {
+    return arr.reduce((partialSum, a) => partialSum + a, 0);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((partialMult, a) => partialMult * a);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let product = 1;
+  for (i = n; i > 0; i--) {
+    product *= i
+
+  }
+  return product;
 };
 
 // Do not edit below this line
