70f109953547863890ad1cc2251401611e9d99ba
Ari <24855858+AriFarj@users.noreply.github.com>
2025-01-02 15:28:08 +0200

    11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objArr) {
+    const titles = objArr.map(book => `${book.title}`)
+    return titles
 };
 
 // Do not edit below this line
