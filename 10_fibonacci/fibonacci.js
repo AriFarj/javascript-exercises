4aa9ff378f0f28eda3a498cf99b4ba6ec13baf78
Ari <24855858+AriFarj@users.noreply.github.com>
2025-01-02 15:23:52 +0200

    10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    if (index < 0) return `OOPS`
+    if (index == 0) return 0;
 
+     index = parseInt(index);
+
+let firstPrev = 1;
+let secondPrev = 0;
+
+    for (i = 2; i <= index; i++){
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
