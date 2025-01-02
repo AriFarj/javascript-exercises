64e3a93b462ce86f1c96c2f0ab00aa090ebda650
Ari <24855858+AriFarj@users.noreply.github.com>
2025-01-02 15:01:36 +0200

    09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+   const validLetters = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
+     const cleanStr = str
+     .toLowerCase()
+     .split('')
+     .filter((char) => validLetters.includes(char))
+     .join('');
+
+     const reversedStr = cleanStr
+     .split('')
+     .reverse()
+     .join('');
+
+     return cleanStr === reversedStr;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
